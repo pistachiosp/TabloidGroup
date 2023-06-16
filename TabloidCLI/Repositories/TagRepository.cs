0a784d34c6cf6bf17feb93cf9efa5c29ba250eef
Laura Furnivall <laurafurnivall@gmail.com>
2023-06-16 09:59:10 -0500

    Finished #24, #25

--- TabloidCLI/Repositories/TagRepository.cs
+++ TabloidCLI/Repositories/TagRepository.cs
@@ -66,12 +66,33 @@ namespace TabloidCLI
 
         public void Update(Tag tag)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Tag
+                                        SET Name = @name
+                                        WHERE id = @id";
+                    cmd.Parameters.AddWithValue("@name", tag.Name);
+                    cmd.Parameters.AddWithValue("@id", tag.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Tag WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
 
         public SearchResults<Author> SearchAuthors(string tagName)
--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -74,12 +74,57 @@ namespace TabloidCLI.UserInterfaceManagers
 
         private void Edit()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Tag to Edit");
+            List<Tag> editTags = _tagRepository.GetAll();
+            foreach (Tag t in editTags)
+            {
+                Console.WriteLine($"{t.Id} - {t.Name}");
+            }
+            Console.Write("Which tag would you like to edit? ");
+            string editTagId = Console.ReadLine();
+            if (editTagId == "")
+            {
+                return;
+            }
+            else 
+            {
+                Console.Write("Pick a new tag name: ");
+                string newTagName = Console.ReadLine();
+
+                Tag tagToEdit = new Tag
+                {
+                    Id = int.Parse(editTagId),
+                    Name = newTagName
+                };
+
+                _tagRepository.Update(tagToEdit);
+
+                Console.WriteLine("Your tag was edited!");
+            }
         }
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Console.WriteLine("Remove Tag");
+            List<Tag> removeTags = _tagRepository.GetAll();
+            foreach (Tag rT in removeTags)
+            {
+                Console.WriteLine($"{rT.Id} - {rT.Name}");
+            }
+            Console.Write("Which tag would you like to remove? ");
+            int removeTagId = int.Parse(Console.ReadLine());
+            try
+            {
+
+                _tagRepository.Delete(removeTagId);
+
+                Console.WriteLine("Tag was successfully removed!");
+            }
+            catch
+            {
+                Console.WriteLine("Cannot remove tag.");
+            }
+
         }
     }
 }
